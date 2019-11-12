5b067fa50841f006cf995407ed8050fe7feb1349
tinashe <giyavha_tinashe@berkeley.edu>
2019-11-11 17:48:29 -0800

    Added working timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,107 @@ function Question(number, questionText, choices, answer) {
     this.choices = choices;
     this.answer = answer;
     this.displayQuestion = function () {
-        // $("#question-container").show();
-        // $("#answer-container").hide();
 
-        // startTimer();
+        questionDiv = $("<form>");
+        questionDiv.text(this.questionText);
 
-        $("#question").text(this.questionText);
-
-        for (let i = 0; i < this.choices.length; i++) {
-            $("#choice-" + i).text(this.choices[i]);
-        }
+        $("#question").append(questionDiv);
+        this.choices.forEach(element => {
+            $("#question").append("<button>" + element + "</button>");
+        });
     }
 }
-let Q1 = new Question(1, "What does Mosi Oa tunya mean?", ["Smoke", "Falls", "Wind", "Rain"], "Smoke");
+Q1 = new Question(1, "What does Mosi Oa tunya mean?", ["Smoke that thunders", "Victoria Falls", "Windy ravine", "Thunderstorms"], "Smoke that thunders");
+Q2 = new Question(2, "Who was the first president of Zimbabwe?", ["Robert Mugabe", "Canaan Banana", "Morgan Tsvangirai", "Abel Muzorewa"], "Canaan Banana");
+Q3 = new Question(3, "In what year did Zimbabwe attain independence from British Colonial Rule?", ["1957", "1980", "2017", "1960"], "1980");
+Q4 = new Question(4, "What is the capital city of Zimbabwe?", ["Harare", "Bulawayo", "Gweru", "Mutare"], "Harare");
 
-$(document).ready(function () {
-    Q1.displayQuestion()
-    console.log(Q1)
-})
+questionSet = [Q1, Q2, Q3, Q4]
+
+// 
+let gameInterval;
+let gametime = 6;
+let timerRunning = false;
+let questionNumber = 0;
+
+function run() {
+    if (!timerRunning) {
+        intervalId = setInterval(decrement, 1000);
+        timerRunning = true;
+    }
+}
+
+function decrement() {
+    
+    gametime--;
+    $("#timer-display").text("Time remaining: " + gametime + " seconds");
+    if (gametime === 0) {
+        alert("Time Up!");
+        stop();
+        questionNumber++;
+        gametime = 5;
+    }
+}
+
+function stop() {
+    clearInterval(gameInterval);
+    timerRunning = false;
+}
+
+
+
+
+
+
+// function game() {
+//     // $("#timer-display").html("<h2>Time remaining: " + gametime + "<h/h2>");
+//     $(".start-display").empty();
+//     questionSet[questionNumber].displayQuestion();
+//     startTimer();
+// }
+
+
+// function startTimer() {
+//     showTime();
+//     clearInterval(gameInterval);
+//     gameInterval = setInterval(game, 1000);
+// }
+
+// function stop() {
+//     clearInterval(gameInterval);
+//     gameInterval = undefined;
+// }
+
+// function results() {
+
+//     let correct = 0;
+//     let wrong = 0;
+//     let unAnswered = 0;
+
+
+
+//     for (let i = 0; i < questionSet.length; i++) {
+
+
+
+//     };
+// }
+
+// function showTime() {
+//     gametime--;
+//     $("#timer-display").text("Time remaining: " + gametime + " seconds");
+
+//     if (gametime === 0) {
+//         stop();
+//         displayAnswer();
+//     }
+// }
+
+// function answer() {
+//     stop();
+//     let answer = $(this).val();
+//     console.log(answer)
+// }
+
+$("#start").on("click", run);
+// $("button").on("click", answer);
